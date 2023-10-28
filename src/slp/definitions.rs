c743f0e3d9eeef6f0dd3fddc0f50c6a86882b94e
heinezen <heinezen@hotmail.de>
2023-10-29 01:29:29 +0200

    slp: Add SLP structs size.

---
+++ src/slp/definitions.rs
@@ -0,0 +1,8 @@
+// Copyright 2023-2023 the slutils-rs authors.
+
+pub const SLP_VERSION_SIZE: usize = 4;
+pub const SLP_HEADER_SIZE: usize = 32;
+
+pub const SLP_FRAME_INFO_SIZE: usize = 32;
+pub const SLP_FRAME_BOUNDS_SIZE: usize = 4;
+pub const SLP_FRAME_CMD_OFFSET_SIZE: usize = 4;
